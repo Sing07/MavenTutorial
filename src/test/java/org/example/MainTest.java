7dbb6a2a0fdf5932dc6fe20db27604c9a2dff721
sing <singwei2@icloud.com>
2023-03-01 17:09:15 -0800

    Initial commit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+    @Test
+    public void TestHypothenuse() {
+        Assertions.assertEquals(Main.hypothenuse(10,20),22.360679774997898);
+    }
+}
